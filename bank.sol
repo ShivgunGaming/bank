eaf82d8af35fcb64b0a47cf674a5e156d13967ad
Shivan <102505925+ShivgunGaming@users.noreply.github.com>
2022-11-01 15:19:13 +1100

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -1,15 +1,53 @@
-pragma solidity 0.8.7;
+pragma solidity 0.8.7; 
 
 contract Bank {
-
-    mapping(address => uint) balance;
-
-    function addBalance(uint _toAdd) public returns(uint) {  
-    balance[msg.sender] += _toAdd;
-    return balance[msg.sender];
+    
+    mapping (address => uint) balance; 
+    address owner; 
+    
+    event depositDone(uint amount, address depositedTo) ;
+    event amountTransferred(uint amount, address toAddress, address fromAddress); //answer
+    modifier onlyOwner {
+        
+        require(msg.sender == owner);
+        _; 
     }
-
-    function getBalance() public view returns(uint) {
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    function deposit() public payable returns (uint) {
+        
+        balance[msg.sender] += msg.value;
+        emit depositDone(msg.value, msg.sender);
         return balance[msg.sender];
     }
+    
+    function getBalance() public view returns (uint) {
+        return balance[msg.sender];
+    }
+    
+    function transfer(address recipient, uint amount) public payable {
+        
+        require(balance[msg.sender] >= amount, "Balance not Sufficient");
+        require(msg.sender != recipient, "Can't transfer money to yourself");
+        
+        uint previousSenderBalance = balance[msg.sender];
+        
+        _transfer(msg.sender, recipient, amount);
+        
+        assert(balance[msg.sender] == previousSenderBalance - amount);
+        
+        emit amountTransferred(amount, recipient, msg.sender); //answer
+        
+    }
+    
+    function _transfer(address from, address to, uint amount) private {
+        
+        balance[from] -= amount; 
+        balance[to] += amount;
+        
+    }
+    
 }
